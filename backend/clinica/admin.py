2a3bd4e7c35ab3bc23d5d6f24d847ffbb9d79bbe
higorifc <higor.tabraga@gmail.com>
2025-06-21 15:52:34 -0300

    FEAT: Criação da API Fornecedor #15

--- backend/clinica/admin.py
+++ backend/clinica/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Medico, Especialidade, Paciente, Atendente, Sala, Consulta, Exame
+from .models import Medico, Especialidade, Paciente, Atendente, Sala, Consulta, Exame, Fornecedor
 
 
-admin.site.register(Medico, Especialidade, Paciente, Atendente, Sala, Consulta, Exame)
+admin.site.register(Medico, Especialidade, Paciente, Atendente, Sala, Consulta, Exame, Fornecedor)
--- backend/clinica/models.py
+++ backend/clinica/models.py
@@ -64,4 +64,13 @@ class Exame(models.Model):
 
     def __str__(self):
         return f"Exame de {self.consulta.paciente.nome} na sala {self.sala} em {self.data.strftime('%d/%m/%Y %H:%M')}"
-    
+
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=80, null=False)
+    cpf = models.CharField(max_length=14, unique=True, null=True)
+    email = models.EmailField(max_length=80, unique=True, null=False)
+    telefone = models.CharField(max_length=19, unique=True, null=False)
+
+    def __str__(self):
+        return f"{self.nome}"
+   
