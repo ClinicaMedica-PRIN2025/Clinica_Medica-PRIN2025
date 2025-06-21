94d58638e5f654cd3b14da16c6a70aea1cf1559a
Pauloartur123 <pauloartur.ifc@gmail.com>
2025-06-21 12:50:56 -0300

    FEAT: Criação de um API Sala #11

--- backend/clinica/models.py
+++ backend/clinica/models.py
@@ -39,3 +39,9 @@ class Atendente(models.Model):
 
     def __str__(self):
         return f"{self.nome}"
+
+class Sala(models.Model):
+    nome_sala = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f'Sala {self.nome_sala}'
