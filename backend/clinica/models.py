27d49e6717ce267d15fe8fbf878ff4b746cc4af3
higorifc <higor.tabraga@gmail.com>
2025-06-21 15:23:02 -0300

    FEAT: Criação do API Consulta #13

--- backend/clinica/models.py
+++ backend/clinica/models.py
@@ -45,3 +45,14 @@ class Sala(models.Model):
 
     def __str__(self):
         return f'Sala {self.nome_sala}'
+    
+class Consulta(models.Model):
+    atendente = models.ForeignKey(Atendente, on_delete=models.PROTECT, null=True)
+    medico = models.ForeignKey(Medico, on_delete=models.PROTECT)
+    paciente = models.ForeignKey(Paciente, on_delete=models.PROTECT)
+    data = models.DateTimeField(null=False)
+    sala = models.ForeignKey(Sala, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Consulta de {self.paciente.nome} com {self.medico.nome}, na sala {self.sala} em {self.data.strftime('%d/%m/%Y %H:%M')}"
+
